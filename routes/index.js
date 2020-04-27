9b900e87e212bb1e7ee784417837aba5a19de8ef
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-26 21:03:46 -0600

    Complete with user authorization

--- routes/index.js
+++ routes/index.js
@@ -24,31 +24,45 @@ function asyncHandler(cb){
   }
 //middleware to Authenticate User
 
-const authenticateUser = (req, res, next) => {
+const authenticateUser = async (req, res, next) => {
     // Parse the user's credentials from the Authorization header.
-    const credentials = auth(req);
+    let credentials = auth(req);
 
-    // If the user's credentials are available...
-    // Attempt to retrieve the user from the data store
-    // by their username (i.e. the user's "key"
-    // from the Authorization header).
-  
-    // If a user was successfully retrieved from the data store...
-    // Use the bcryptjs npm package to compare the user's password
-    // (from the Authorization header) to the user's password
-    // that was retrieved from the data store.
-  
-    // If the passwords match...
-    // Then store the retrieved user object on the request object
-    // so any middleware functions that follow this middleware function
-    // will have access to the user's information.
-  
-    // If user authentication failed...
-    // Return a response with a 401 Unauthorized HTTP status code.
-  
-    // Or if user authentication succeeded...
-    // Call the next() method.
-  };
+    // match given username with availableconst authenticateUser = (req, res, next) => {
+  let message = null;
+
+  if (credentials) {
+    // Look for a user whose `username` matches the credentials `name` property.
+    //const user = await User.findOne({ where: { emailAddress : credentials.name }});
+    const users = await User.findAll();
+    const user = users.find(user => user.emailAddress === credentials.name);
+
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.username}`);
+
+        // Store the user on the Request object.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
+
   
   //send a GET request to view all courses
 router.get('/courses', asyncHandler(async (req, res) =>{
@@ -124,7 +138,7 @@ router.get('/courses/:id', asyncHandler(async (req, res) =>{
 }))
 
 //sends a POST request to create a new course
-router.post('/courses', [titleValidationChain, userIdValidationChain],
+router.post('/courses', [titleValidationChain, userIdValidationChain], authenticateUser,
   asyncHandler(async (req, res) =>{
   //'course' body sent & tested via Postman in the following format:
   // {
